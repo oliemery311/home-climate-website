ef67c7ee6851f61702772546c4d26f78e085ad80
oliemery311 <oliemery311@gmail.com>
2026-07-26 11:23:55 +0100

    Prepare Cloudflare deployment configuration

--- .open-next/server-functions/default/.next/server/app/api/quotes/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/quotes/route.js")
-R.c("server/chunks/[externals]_next_dist_0iuj5m_._.js")
-R.c("server/chunks/[root-of-the-server]__0xuaoik._.js")
-R.c("server/chunks/node_modules_next_dist_esm_build_templates_app-route_0ns-ue4.js")
-R.c("server/chunks/_next-internal_server_app_api_quotes_route_actions_0pk0ee2.js")
-R.m(41332)
-module.exports=R.m(41332).exports
--- .open-next/server-functions/default/.next/server/chunks/_next-internal_server_app_api_quotes_route_actions_0pk0ee2.js
+++
@@ -1,3 +0,0 @@
-module.exports=[23450,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_quotes_route_actions_0pk0ee2.js.map
