14d2118349c5341e0b5163cc8f4636c3e7648473
oliemery311 <oliemery311@gmail.com>
2026-07-28 22:00:13 +0100

    favicon logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,14 @@ export const metadata: Metadata = {
     "multi split systems",
   ],
 
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-icon.png",
+  },
   openGraph: {
     title:
       "Home Climate Systems | Residential Air Conditioning",
